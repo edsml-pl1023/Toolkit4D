718ed7b24f8ec850cb621e23468a5e7525358f7d
edsml-pl1023 <pl1023@ic.ac.uk>
2024-07-04 20:31:48 +0100

    Add testing for threshold_rock

--- tests/test_threshold_rock.py
+++ tests/test_threshold_rock.py
@@ -6,56 +6,101 @@ import glob
 import numpy as np
 
 
+def test_data_match():
+    """
+    First, test number of .mat threshold info are the same as number of raw
+    images in test_data
+    Second, test they exactly correspond to a same 'image'; they exactly match
+    by two direction assert
+    """
+    # load mat data
+    mat_data = loadmat('./tests/test_data/threshold_values.mat')
+    threshold_infos = []
+    for rock_label in mat_data['rock_value'].dtype.names:
+        for day_label in mat_data['rock_value'][rock_label][0][0].dtype.names:
+            threshold_value = int(
+                mat_data['rock_value'][rock_label][0][0][day_label]
+                )
+            # threshold info is a list of tuples, each tuple:
+            # (rock label, day label, thres value)
+            threshold_infos.append((rock_label, day_label, threshold_value))
+    threshold_infos_label = [info[0]+info[1] for info in threshold_infos]
+
+    # load raw images
+    test_data_path = './tests/test_data/'
+    test_raw_path = os.path.join(test_data_path, '*.raw')
+    raw_files = glob.glob(test_raw_path)
+
+    assert len(threshold_infos) == len(raw_files)
+
+    mat_variables = []
+    for raw_file in raw_files:
+        clean_path = os.path.basename(raw_file)
+        file_name = os.path.splitext(clean_path)[0]
+        fnparts = file_name.split('_')
+        mat_variable = fnparts[0]+fnparts[2]+fnparts[1]
+        mat_variables.append(mat_variable)
+        assert mat_variable in threshold_infos_label
+
+    for threhold_info_label in threshold_infos_label:
+        assert threhold_info_label in mat_variables
+
+
 def test_thresholding_mse():
     """
     Test the mse between thresholding using python threshold_rocks
     and Matlab threshold_rocks (as Ground Truth)
     Mse within a range is acceptable
     """
+    mat_data = loadmat('./tests/test_data/threshold_values.mat')
+    threshold_infos = []
+    for rock_label in mat_data['rock_value'].dtype.names:
+        for day_label in mat_data['rock_value'][rock_label][0][0].dtype.names:
+            threshold_value = int(
+                mat_data['rock_value'][rock_label][0][0][day_label]
+                )
+            # threshold info is a list of tuples, each tuple:
+            # (rock label, day label, thres value)
+            threshold_infos.append((rock_label, day_label, threshold_value))
 
-
-def test_data_match():
-    """
-    First, test number of .mat variables are the same as number of raw
-    images in test_data
-    Second, test they exactly correspond to a same 'image'
-    """
-    mat_data = loadmat('./tests/test_data/downsampled_raw_image.mat')
-    # count the number of meta data in .mat
-    meta = sum(1 for key in mat_data.keys() if key.startswith('_'))
-    test_data_path = './tests/test_data/'
-    test_raw_path = os.path.join(test_data_path, '*.raw')
-    raw_files = glob.glob(test_raw_path)
-    assert len(mat_data)-meta == len(raw_files)
-    for raw_file in raw_files:
-        clean_path = os.path.basename(raw_file)
-        file_name = os.path.splitext(clean_path)[0]
-        fnparts = file_name.split('_')
-        mat_variable = '_'.join(fnparts[:3])
-        assert mat_variable in list(mat_data.keys())
-
-
-def test_readraw():
-    """
-    Test if matlab raw-image variables are the same as python
-    read_raw() result
-    """
-    mat_data = loadmat('./tests/test_data/downsampled_raw_image.mat')
-
+    # load raw images
     test_data_path = './tests/test_data/'
     test_raw_path = os.path.join(test_data_path, '*.raw')
     raw_files = glob.glob(test_raw_path)
 
+    # use these three lists to maintain index info for raw_files
+    mat_variables = []
+    imgs_size = []
+    imgs_type = []
     for raw_file in raw_files:
         clean_path = os.path.basename(raw_file)
         file_name = os.path.splitext(clean_path)[0]
         fnparts = file_name.split('_')
-        mat_variable = '_'.join(fnparts[:3])
-        img_matlab = mat_data[mat_variable]
-        img_python = dataio.read_raw(
-            raw_file,
-            [int(fnparts[5]), int(fnparts[6]), int(fnparts[7])],
-            fnparts[3])
-        print(img_matlab.shape, img_python.shape)
+        mat_variable = fnparts[0]+fnparts[2]+fnparts[1]
+        mat_variables.append(mat_variable)
+        imgs_size.append([int(fnparts[5]), int(fnparts[6]), int(fnparts[7])])
+        imgs_type.append(fnparts[3])
 
-        assert np.array_equal(img_python, img_matlab)
+    for threshold_info in threshold_infos:
+        raw_file = raw_files[mat_variables.index(
+            threshold_info[0]+threshold_info[1]
+            )]
+        img_size = imgs_size[mat_variables.index(
+            threshold_info[0]+threshold_info[1]
+            )]
+        img_type = imgs_type[mat_variables.index(
+            threshold_info[0]+threshold_info[1]
+            )]
+
+        ret_python = thresholding.threshold_rock(raw_file)
+        ret_matlab = threshold_info[2]
+
+        # load data
+        img_raw = dataio.read_raw(raw_file, img_size, img_type)
+
+        # apply thresholding
+        binary_matlab = img_raw <= ret_matlab
+        binary_python = img_raw <= ret_python
+        difference = binary_matlab ^ binary_python
+        relative_error = np.sum(difference) / np.prod(img_size)
+        assert relative_error <= 0.05
