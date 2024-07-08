9a2fad5b4bf1fead07a4092195debe6b79f8f053
edsml-pl1023 <pl1023@ic.ac.uk>
2024-07-08 16:25:22 +0100

    basic structure for pipeline class

--- ToolKit4D/pipeline.py
+++ ToolKit4D/pipeline.py
@@ -1,10 +1,33 @@
-from ToolKit4D.dataio import read_raw
-from ToolKit4D.thresholding import threshold_rock
-from ToolKit4D.utils import remove_cylinder
+import ToolKit4D.dataio as dio
+import ToolKit4D.thresholding as thresh
+import ToolKit4D.utils as ut
+import os
 
 
+# 1. write the basic structure
+# 2. add clean ram inside class and compare the saving of ram
+# 3. add lines to write intermediate result into disk
 class ToolKitPipeline:
     """_summary_: processing per image per instance
     """
-    def __init__(self):
-        pass
+    def __init__(self, rawfile):
+        """_summary_
+
+        Args:
+            rawfile_path (_type_): complete path to raw file
+        """
+        self.rawfile = rawfile
+        clean_path = os.path.basename(rawfile)
+        file_name = os.path.splitext(clean_path)[0]
+        fnparts = file_name.split('_')
+        self.identifier = fnparts[0] + fnparts[2] + fnparts[1]
+        self.im_size = [int(fnparts[5]), int(fnparts[6]), int(fnparts[7])]
+        self.im_type = fnparts[3]
+        self.raw = self._read_raw()
+
+    def _read_raw(self):
+        raw = dio.read_raw(self.rawfile, self.im_size, self.im_type)
+        return raw
+
+    def threshold_rock(self):
+        self.rockThresh = thresh.threshold_rock(raw_image=self.raw)
