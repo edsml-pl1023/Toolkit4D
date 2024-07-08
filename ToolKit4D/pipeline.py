39cb4f06f122482196ad10c33bf7eb19c8e01ab9
edsml-pl1023 <pl1023@ic.ac.uk>
2024-07-08 15:22:29 +0100

    Add pipeline class

---
+++ ToolKit4D/pipeline.py
@@ -0,0 +1,10 @@
+from ToolKit4D.dataio import read_raw
+from ToolKit4D.thresholding import threshold_rock
+from ToolKit4D.utils import remove_cylinder
+
+
+class ToolKitPipeline:
+    """_summary_: processing per image per instance
+    """
+    def __init__(self):
+        pass
